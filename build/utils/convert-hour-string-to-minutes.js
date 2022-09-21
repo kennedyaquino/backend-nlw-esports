f33fa833f5bd7e8bfede3b74e5e5d7e69011acdf
kennedyaquino <kennedy.eletronica.94@gmail.com>
2022-09-20 21:36:53 -0300

    sadhsaijdh2173618

---
+++ build/utils/convert-hour-string-to-minutes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.convertHoursStringToMinutes = void 0;
+function convertHoursStringToMinutes(hourString) {
+    const [hours, minutes] = hourString.split(':').map(Number);
+    const minutesAmount = (hours * 60) + minutes;
+    return minutesAmount;
+}
+exports.convertHoursStringToMinutes = convertHoursStringToMinutes;
